ab65c4eee1713ca69fef99bbc3791dc0c0a91c99
Stefano Bassi <127448771+BassiStefano@users.noreply.github.com>
2025-03-10 12:55:13 +0100

    Update SQLQuery4.sql

--- 1-SQL/1-Data Cleaning/SQLQuery4.sql
+++ 1-SQL/1-Data Cleaning/SQLQuery4.sql
@@ -9,5 +9,5 @@ QUERYR 1.4
 SELECT *
 INTO [dbo].[FINAL_complessiva] 
 FROM [dbo].[FINAL_sales] 
-INNER JOIN [dbo].[FINAL_details] ON [dbo].[FINAL_sales].car_id_sales = [dbo].[FINAL_details].car_id_details
-INNER JOIN [dbo].[FINAL_claims]  ON [dbo].[FINAL_sales].car_id_sales = [dbo].[FINAL_claims].car_id_claims
+     INNER JOIN [dbo].[FINAL_details] ON [dbo].[FINAL_sales].car_id_sales = [dbo].[FINAL_details].car_id_details
+     INNER JOIN [dbo].[FINAL_claims]  ON [dbo].[FINAL_sales].car_id_sales = [dbo].[FINAL_claims].car_id_claims
