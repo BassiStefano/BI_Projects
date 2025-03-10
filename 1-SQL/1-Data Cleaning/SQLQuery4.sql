79aca91692a4e8f90a78ca2907ddb98afc599341
Stefano Bassi <127448771+BassiStefano@users.noreply.github.com>
2025-03-10 21:28:37 +0100

    Add files via upload

---
+++ 1-SQL/1-Data Cleaning/SQLQuery4.sql
@@ -0,0 +1,13 @@
+
+
+/* 
+We have cleaned the three tables so we can join them to get the final table
+On which I can perform the analysis
+*/
+
+QUERYR 1.4
+SELECT *
+INTO [dbo].[FINAL_complessiva] 
+FROM [dbo].[FINAL_sales] 
+INNER JOIN [dbo].[FINAL_details] ON [dbo].[FINAL_sales].car_id_sales = [dbo].[FINAL_details].car_id_details
+INNER JOIN [dbo].[FINAL_claims]  ON [dbo].[FINAL_sales].car_id_sales = [dbo].[FINAL_claims].car_id_claims
